e997eb7825737724b349de3afd25f598966a9e23
datng <datnh2@vng.com.vn>
2022-06-17 03:33:42 +0700

    Add main script

--- ImageCompress.py
+++ ImageCompress.py
@@ -1,8 +1,20 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 
-from utils import Reader
+from utils import Writer, Reader, Clustering
 
 if __name__ == "__main__":
-    print(Reader.GetData())
+    filePath, numColors, imageType = Reader.GetData()
+    # filePath = "./data/image01.jpg"
+    # numColors = 5
+    # imageType = "png"
+
+    filename = Reader.parseFileName(filePath)
+
+    imageRawFlat, width, height = Reader.ReadImage(filePath)
+    
+    centers, labels = Clustering.kmeans(imageRawFlat, numColors)
+    for i in range(len(labels)):
+        imageRawFlat[i] = centers[labels[i]]
+
+    Writer.CreateNewImage(imageRawFlat.reshape(height, width, -1), numColors, imageType, filename)
+
