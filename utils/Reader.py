1abd69dc0c64c7b9eb4999d70d56492b2fc1cce2
datng <datnh2@vng.com.vn>
2022-06-17 03:32:34 +0700

    Update Reader

--- utils/Reader.py
+++ utils/Reader.py
@@ -1,5 +1,28 @@
+from PIL import Image
+import numpy as np
+import os.path
+
 def GetData():
     filePath = input("File path: ")
-    k = int(input("Compress to (number of colors): "))
 
-    return filePath, k
+    if not os.path.exists(filePath):
+        print("--- [ERROR] File path is not exist")
+        exit()
+
+    k = int(input("Compress to (number of colors): "))
+    
+    imageType = input("Image type (png, jpg, jpeg): ")
+    if imageType not in ["png", "jpg", "jpeg"]:
+        print("--- [ERROR] Image type is invalid")
+        exit()
+
+    return filePath, k, imageType
+
+
+def parseFileName(path): return ''.join(path.split('/')[-1].split('.')[:-1])
+
+
+def ReadImage(file):
+    im = Image.open(file)
+    width, height = im.size
+    return np.array(im.getdata()), width, height
